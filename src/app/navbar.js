cdd275f16c988c1d071b1a23d47fa41ded0a6478
pankaj ram pawar <pankajpawars123@gmail.com>
2023-12-16 00:12:32 +0530

    landing page content added

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
         setIsMenuOpen(prev => !prev);
     }
 
-    return <main className='flex justify-between mt-5 ml-8 items-center '>
+    return <main className='flex justify-between mt-5 xl:mt-14 ml-8 items-center '>
         <div className='flex items-center justify-between w-full mr-8 md:w-fit'
             onClick={toggleMenu}
         >
@@ -19,7 +19,7 @@ export default function Navbar() {
         </div>
 
         <div 
-            className={`fixed right-0 top-0 w-2/3 h-full md:h-auto pt-28 md:pt-8 gap-8 flex-col flex md:flex-row md:gap-12 menu md:justify-center md:items-center p-8 md:w-1/2 ${isMenuOpen ? 'block' : 'hidden'} md:flex min-w-fit`}
+            className={`absolute right-0 xl:top-10  top-0 w-2/3 h-full md:h-auto pt-28 md:pt-8 gap-8 flex-col flex md:flex-row md:gap-12 menu md:justify-center md:items-center p-8 md:w-1/2 ${isMenuOpen ? 'block' : 'hidden'} md:flex min-w-fit`}
             >
             <div
                 className='fixed top-8 right-8 md:hidden'
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import Navbar from './navbar'
 
 export default function Home() {
@@ -7,13 +6,28 @@ export default function Home() {
   const bgTablet = "/assets/home/background-home-tablet.jpg";
   const bgDesktop = "/assets/home/background-home-desktop.jpg";
 
-  return <div>
+  return <div className='h-screen xl:overflow-hidden'>
     <img src={bgMobile} className='sm:hidden  fixed z-0 top-0 w-full h-full'/>
     <img src={bgTablet} className='hidden sm:block lg:hidden  fixed z-0 top-0 w-full h-full'/>
     <img src={bgDesktop} className='hidden lg:block fixed z-0 top-0 w-full h-full'/>
 
-    <div className='relative z-10'>
+    <div className='z-20 relative top-0 w-full'>
       <Navbar/>
     </div>
+
+    <main className='relative z-10 h-full w-full flex flex-col xl:flex-row justify-evenly items-center'>
+      
+      <section className='flex flex-col items-center xl:items-start text-center xl:text-start gap-6 lg:gap-10 max-w-[450px] p-5'>
+        <p className='text-base sm:text-lg'>SO, YOU WANT TO TRAVEL TO</p>
+        <h1 className='text-7xl sm:text-8xl xl:text-9xl'>SPACE</h1>
+        <p className='sm:text-sm text-xs'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+      </section>
+      
+      <section>
+        <div className='bg-white aspect-square h-64 rounded-full text-black text-4xl flex justify-center items-center'>
+          Explore
+        </div>
+      </section>
+    </main>
   </div>
 }
