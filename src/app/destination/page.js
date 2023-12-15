0392e43e5410b88aeaa39e99f76b810677a0182b
pankaj ram pawar <pankajpawars123@gmail.com>
2023-12-16 01:19:46 +0530

    components conditioning added

--- src/app/destination/page.js
+++ src/app/destination/page.js
@@ -5,7 +5,39 @@ import React from "react"
 
 export default function Destination () {
     
-    const [selection, setSelection] = React.useState(1)
+    const [selection, setSelection] = React.useState(2)
+
+    const planets = {
+        1: {
+            title: "MOON",
+            details:"See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
+            distance: "384,400 KM",
+            time: "3 DAYS"
+        },
+
+        2: {
+            title: "MARS",
+            details: "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
+            distance: "225 mil. KM",
+            time: "9 MONTHS",
+        },
+
+        3: {
+            title: "EUROPA",
+            details: "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
+            distance: "628 mil. KM",
+            time: "3 YEARS",
+        },
+
+        4: {
+            title: "TITAN",
+            details: "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
+            distance: "1.6 bil. KM",
+            time: "7 YEARS",
+        } 
+    }
+
+    const selectedPlanet = planets[selection];
 
     const bgMobile = "/assets/destination/background-destination-mobile.jpg"
     const bgTablet =  "/assets/destination/background-destination-tablet.jpg"
@@ -29,27 +61,30 @@ export default function Destination () {
             </div>
             
             <div>
-                <img src={`/assets/destination/image-moon.png`} />
+                <img src={`/assets/destination/image-${selectedPlanet.title.toLowerCase()}.png`} />
             </div>
 
             <div>
                 <div>
                     <sapn>MOON</sapn>
+                    <span>MARS</span>
+                    <span>EUROPA</span>
+                    <span>TITAN</span>
                 </div>
 
-                <h1>moon title</h1>
+                <h1>{selectedPlanet.title}</h1>
 
-                <p>moon information</p>
+                <p>{selectedPlanet.details}</p>
 
                 <div>
                     <p>
                         <span>AVG. DISTANCE</span>
-                        <span>DISTANCE</span>
+                        <span>{selectedPlanet.distance}</span>
                     </p>
 
                     <p>
                         <span>EST. TRAVEL TIME</span>
-                        <span>timeToTravel</span>
+                        <span>{selectedPlanet.time}</span>
                     </p>
                 </div>
             </div>
