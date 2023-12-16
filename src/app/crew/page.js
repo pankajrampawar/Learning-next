ae1c7442f8e0af414419af655129b97db7be1bc2
pankaj ram pawar <pankajpawars123@gmail.com>
2023-12-16 20:13:32 +0530

    crew page dekstop syle conditions added

--- src/app/crew/page.js
+++ src/app/crew/page.js
@@ -10,7 +10,7 @@ export default function Crew() {
     const crewMembers = {
         1: {
             role: "COMMANDER",
-            name: "Douglas Hurley",
+            name: "DOUGLAS HURLEY",
             details: "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
             imagePath: "/assets/crew/image-douglas-hurley.png"
         },
@@ -22,13 +22,13 @@ export default function Crew() {
         },
         3: {
             role: "PILOT",
-            name: "Victor Glover",
+            name: "VICTOR GLOVER",
             details: "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. ",
             imagePath: "/assets/crew/image-victor-glover.png"
         },
         4: {
             role: "Flight Engineer",
-            name: "Anousheh Ansari",
+            name: "ANOUSHEH ANSARI",
             details: "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space. ",
             imagePath: "/assets/crew/image-anousheh-ansari.png"
         }
@@ -48,28 +48,28 @@ export default function Crew() {
             <Navbar/>
         </nav>
         
-        <div className="flex flex-col items-center text-center gap-8 mt-12 md:mx-[40px]">
-            <section className="self-start">
-                <h2 className="text-lg flex gap-2 justify-center ">
+        <div className="flex flex-col items-center text-center gap-8 mt-12 md:mx-[40px] xl:mx-[165px] xl:items-start">
+            <section className="md:self-start">
+                <h2 className="text-lg flex gap-2 justify-center xl:text-2xl">
                     <span className="font-bold">02</span> <span>MEET YOUR CREW</span>
                 </h2>
             </section>
 
-            <section className="flex flex-col gap-6 items-center px-5">
-                <div className="order-2 sm:order-1 flex flex-col">
-                    <h2 className="order-2 sm:order-1 mt-8 text-xl">{selectedCrewMember.role}</h2>
-                    <h1 className="order-3 sm:order-2 sm:mt-2 text-4xl sm:text-5xl">{selectedCrewMember.name}</h1>
-                    <p className="order-4 sm:order-3 mt-6 text-sm leading-relaxed sm:leading-loose md:h-[84px]">{selectedCrewMember.details}</p>
-                    <div className="flex gap-4 justify-center order-1 sm:order-4 sm:mt-10">
-                        <div className={`w-2.5 h-2.5 block ${selection === 1 ? 'bg-white' : 'bg-slate-600'} rounded-full cursor-pointer`} onClick={()=>{setSelection(1)}}></div>
-                        <div className={`w-2.5 h-2.5 block ${selection === 2 ? 'bg-white' : 'bg-slate-600'} rounded-full cursor-pointer`} onClick={()=>{setSelection(2)}}></div>
-                        <div className={`w-2.5 h-2.5 block ${selection === 3 ? 'bg-white' : 'bg-slate-600'} rounded-full cursor-pointer`} onClick={()=>{setSelection(3)}}></div>
-                        <div className={`w-2.5 h-2.5 block ${selection === 4 ? 'bg-white' : 'bg-slate-600'} rounded-full cursor-pointer`} onClick={()=>{setSelection(4)}}></div>
+            <section className="flex flex-col gap-6 xl:gap-0 items-center px-5 xl:flex-row xl:justify-between xl:px-0 xl:w-full">
+                <div className="order-2 sm:order-1 flex flex-col xl:items-start xl:text-left xl:justify-between">
+                    <h2 className="order-2 sm:order-1 mt-8 text-xl xl:text-3xl">{selectedCrewMember.role}</h2>
+                    <h1 className="order-3 sm:order-2 sm:mt-2 text-4xl sm:text-5xl xl:text-[56px]">{selectedCrewMember.name}</h1>
+                    <p className="order-4 sm:order-3 mt-6 text-sm xl:text-lg leading-relaxed sm:leading-loose md:h-[84px] xl:h-auto  xl:w-[444px]">{selectedCrewMember.details}</p>
+                    <div className="flex gap-4 justify-center order-1 sm:order-4 sm:mt-10 xl:mt-24">
+                        <div className={`w-2.5 h-2.5 xl:w-4 xl:h-4 block ${selection === 1 ? 'bg-white' : 'bg-slate-600'} rounded-full cursor-pointer`} onClick={()=>{setSelection(1)}}></div>
+                        <div className={`w-2.5 h-2.5 xl:w-4 xl:h-4 block ${selection === 2 ? 'bg-white' : 'bg-slate-600'} rounded-full cursor-pointer`} onClick={()=>{setSelection(2)}}></div>
+                        <div className={`w-2.5 h-2.5 xl:w-4 xl:h-4 block ${selection === 3 ? 'bg-white' : 'bg-slate-600'} rounded-full cursor-pointer`} onClick={()=>{setSelection(3)}}></div>
+                        <div className={`w-2.5 h-2.5 xl:w-4 xl:h-4 block ${selection === 4 ? 'bg-white' : 'bg-slate-600'} rounded-full cursor-pointer`} onClick={()=>{setSelection(4)}}></div>
                     </div>
                 </div>
 
-                <div className="order-1 sm:order-2 border-b sm:border-b-0 border-gray-500 w-full mx-10 flex justify-center">
-                    <img src={selectedCrewMember.imagePath} alt={`${selectedCrewMember.name}`} className="max-h-[223px] md:max-h-[572px]"/>
+                <div className="order-1 sm:order-2 border-b sm:border-b-0 border-gray-500 w-full xl:w-auto mx-10 xl:mx-0 flex justify-center xl:justify-end">
+                    <img src={selectedCrewMember.imagePath} alt={`${selectedCrewMember.name}`} className="max-h-[223px] md:max-h-[572px] xl:max-h-fit"/>
                 </div>
             </section>
         </div>
